2de62cc718e3509d5337e8df79dd62f21ba0d515
Asherbeck10 <asher.bec@gmail.com>
2024-06-07 18:40:14 +0100

    fixing lint config

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,26 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
+  languageOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    rules: {
-      indent: ['error', 2], // enforce consistent indentation
-      'linebreak-style': ['error', 'unix'], // enforce consistent linebreak style
-      quotes: ['error', 'single'], // enforce the consistent use of either backticks, double, or single quotes
-      semi: ['error', 'always'], // require or disallow semicolons instead of ASI
-      'no-unused-vars': ['error'], // disallow unused variables
-      'no-console': 'off', // disallow the use of console
-      eqeqeq: 'error', // require the use of === and !==
-      curly: 'error', // enforce consistent brace style for all control statements
-      '@typescript-eslint/explicit-module-boundary-types': 'off', // Require explicit return and argument types on exported functions' and classes' public class methods
-    },
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['error'],
+    'no-console': 'off',
+    eqeqeq: 'error',
+    curly: 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
