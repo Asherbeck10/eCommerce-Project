a14307a3e5ba16dc722e577706970321ea8c6de8
Asherbeck10 <asher.bec@gmail.com>
2024-06-07 16:46:32 +0100

    no add to cart if out of stock

--- client/src/pages/shop/Product.tsx
+++ client/src/pages/shop/Product.tsx
@@ -22,15 +22,20 @@ export default function Product({ product }: Prop) {
       <h3>{productName}</h3>
       <p>{description}</p>
       <p>£{price}</p>
-      <button className={styles.addToCartBttn} onClick={() => addToCart(_id)}>
-        Add to cart
-        {/* Show the number of items in the cart if it is greater than 0 */}
-        {count > 0 && <>({count})</>}
-      </button>
 
       {/* Show the stock quantity if it is greater than 0 */}
       {stockQuantity > 0 ? (
-        <p className={styles.stock}>In stock: {stockQuantity}</p>
+        <>
+          <button
+            className={styles.addToCartBttn}
+            onClick={() => addToCart(_id)}
+          >
+            Add to cart
+            {/* Show the number of items in the cart if it is greater than 0 */}
+            {count > 0 && <>({count})</>}
+          </button>
+          <p className={styles.stock}>In stock: {stockQuantity}</p>
+        </>
       ) : (
         <p className={styles.stock}>Out of stock</p>
       )}
